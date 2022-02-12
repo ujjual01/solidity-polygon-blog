5c75d8b60f7533a1208e36b5fcb4359f6906ed78
ujjual <ujjual72@gmail.com>
2022-02-12 20:17:43 +0530

    tested

--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -12,25 +12,25 @@ contract Blog {
     Counters.Counter private _postIds;
 
     struct Post {
-        uint id;
+        uint256 id;
         string title;
         string content;
         bool published;
     }
 
-    mapping(uint => Post) private idToPost;
+    mapping(uint256 => Post) private idToPost;
     mapping(string => Post) private hashToPost;
 
-    event PostCreated(uint id,string title,string hash);
-    event PostUpdated(uint id,string title,string hash,bool published);
+    event PostCreated(uint256 id, string title, string hash);
+    event PostUpdated(uint256 id, string title, string hash, bool published);
 
-    constructor(string memory _name){
-        console.log("Deploying with name":_name);
+    constructor(string memory _name) {
+        console.log("Deploying with name", _name);
         name = _name;
         owner = msg.sender;
     }
 
-    function updateName(string memory _name) pubic {
+    function updateName(string memory _name) public {
         name = _name;
     }
 
@@ -43,23 +43,31 @@ contract Blog {
         owner = newOwner;
     }
 
-    function fetchPost(string memory hash) public view returns(Post memory){
+    function fetchPost(string memory hash) public view returns (Post memory) {
         return hashToPost[hash];
     }
 
-    function createPost(string memory title,string memory hash) public onlyOwner {
+    function createPost(string memory title, string memory hash)
+        public
+        onlyOwner
+    {
         _postIds.increment();
-        uint postId = _postIds.current();
+        uint256 postId = _postIds.current();
         Post storage post = idToPost[postId];
         post.id = postId;
         post.title = title;
         post.published = true;
         post.content = hash;
         hashToPost[hash] = post;
-        emit PostCreated(postId,title,hash);
+        emit PostCreated(postId, title, hash);
     }
 
-    function updatePost(uint postId, string memory title,string memory hash,bool published) public onlyOwner {
+    function updatePost(
+        uint256 postId,
+        string memory title,
+        string memory hash,
+        bool published
+    ) public onlyOwner {
         Post storage post = idToPost[postId];
         post.title = title;
         post.published = published;
@@ -69,18 +77,17 @@ contract Blog {
         emit PostUpdated(post.id, title, hash, published);
     }
 
-    function fetchPosts() public view returns (Post[] memory){
-        uint itemCount = _postIds.current();
-        uint currentIndex = 0;
+    function fetchPosts() public view returns (Post[] memory) {
+        uint256 itemCount = _postIds.current();
+        uint256 currentIndex = 0;
 
         Post[] memory posts = new Post[](itemCount);
-        for(uint i=0;i<itemCount;i++){
-            uint currentId = i+1;
+        for (uint256 i = 0; i < itemCount; i++) {
+            uint256 currentId = i + 1;
             Post storage currentItem = idToPost[currentId];
             posts[currentIndex] = currentItem;
             currentIndex += 1;
         }
         return posts;
     }
-
 }
